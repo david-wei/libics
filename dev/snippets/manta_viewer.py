e53ffa072933128ca02bc2c498c786681ff9cf12
Admin <Admin>
2018-08-24 16:14:13 +0200

    manta_viewer: verbose

--- dev/snippets/manta_viewer.py
+++ dev/snippets/manta_viewer.py
@@ -31,6 +31,9 @@ def setup_camera(camera):
     camera.set_acquisition(mode="Continuous")
     camera.set_format(width="max", height="max", px_format="Mono8")
     camera.set_exposure(auto="Continuous")
+    print("Acquisition:", camera.get_acquisition())
+    print("Format:", camera.get_format())
+    print("Exposure:", camera.get_exposure())
     # Configure Vimba API
     camera.set_frame_buffers(count=3)
     return camera
@@ -41,7 +44,7 @@ def setup_video(camera):
     Sets up OpenCV video display of camera.
     """
     v = cvimage.Video()
-    v.set_playback_mode(True, interval=25)
+    v.set_playback_mode(True, interval=20)
     v.set_source_mode("frame_function", frame_function=camera.get_image)
     return v
 
@@ -50,13 +53,21 @@ def play(camera, video):
     """
     Live-displays the camera image.
     """
+    print("Open cap")
     video.open_cap()
+    print("Start capture")
     camera.start_capture()
+    print("Start acquisition")
     camera.start_acquisition()
+    print("Play stream")
     video.play(wnd_title="Press s to stop stream", break_key="s")
+    print("End acquisition")
     camera.end_acquisition()
+    print("End capture")
     camera.end_capture()
+    print("Close cap")
     video.close_cap()
+    print("Finished playback")
 
 
 def cleanup(camera):
