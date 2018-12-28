709de0e38a8c381ba5bf3eec60397150bae34d2e
David Wei <david94.wei@gmail.com>
2018-12-28 21:10:35 +0100

    trafo: Add linear

--- libics/trafo/__init__.py
+++ libics/trafo/__init__.py
@@ -1,5 +1,6 @@
 from . import data          # noqa
 from . import fourier       # noqa
 from . import fourierutil   # noqa
+from . import linear        # noqa
 from . import resize        # noqa
 from . import sigproc       # noqa
