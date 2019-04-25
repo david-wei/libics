ee41991df915e5ea158945a8e9aeac64ebef2852
David Wei <david94.wei@gmail.com>
2019-04-25 18:57:08 +0200

    trafo: Remove legacy

--- libics/trafo/fourier.py
+++ libics/trafo/fourier.py
@@ -1,69 +1,14 @@
 # System Imports
-import copy
 import numpy as np
 
 # Package Imports
-from libics.cfg import err as ERR
-from libics.data import matrixdata as mdata
-from libics.data import types, arraydata
+from libics.data import arraydata
 
 # Subpackage Imports
 from libics.trafo import fourierutil
 
 
 ###############################################################################
-
-
-def fft_matrixdata(data, mode=None):
-    """
-    Computes the Fast Fourier Transform of a `data.matrixdata.MatrixData`
-    object.
-
-    Parameters
-    ----------
-    data : data.matrixdata.MatrixData
-        The matrix data to be Fourier transformed.
-    mode : None or str
-        None: Saves full complex values of Fourier transform.
-        "abs": Saves spectrum (modulus of Fourier transform).
-        "phase": Saves phase of Fourier transform.
-        "re": Saves real part of Fourier transform.
-        "im": Saves imaginary part of Fourier transform.
-
-    Returns
-    -------
-    data : data.matrixdata.MatrixData
-        Fourier transformed data.
-
-    Raises
-    ------
-    cfg.err.DTYPE_MATRIXDATA, cfg.err.INVAL_SET
-        If parameters are invalid.
-    """
-    ERR.assertion(ERR.DTYPE_MATRIXDATA,
-                  type(data) == mdata.MatrixData)
-    # Perform FFT
-    ft_data = np.fft.fftshift(np.fft.fftn(data.get_val_data()))
-    if mode is None:
-        pass
-    elif mode == "abs":
-        ft_data = np.abs(ft_data)
-    elif mode == "phase":
-        ft_data = np.angle(ft_data)
-    elif mode == "re":
-        ft_data = np.real(ft_data)
-    elif mode == "im":
-        ft_data = np.imag(ft_data)
-    else:
-        raise ERR.INVAL_SET(str(ERR.INVAL_SET))
-    # Set matrixdata
-    pq_x, pq_y, pq_val = copy.deepcopy(data.get_pquants())  # pquant is mutable
-    pq_val, (pq_x, pq_y) = fourierutil.ft_pquants(pq_val, pq_x, pq_y)
-    ft_mrect = fourierutil.ft_matrixrect(data.var_rect)
-    data.set_val_data(ft_data, val_pquant=pq_val)
-    data.set_var_data(matrix_rect=ft_mrect,
-                      var_x_pquant=pq_x, var_y_pquant=pq_y)
-    return data
 
 
 def fft_arraydata(data, mode=None):
@@ -128,28 +73,6 @@ if __name__ == "__main__":
     # Test function
     def f(x):
         return np.cos(2 * np.pi * x) + np.sin(5 * np.pi * x)
-
-    # -----------
-    # Matrix data
-    # -----------
-    array = np.full((6, 6), 0.0)
-    for x in range(array.shape[0]):
-        for y in range(array.shape[1]):
-            array[x, y] = np.sqrt(x**2 + y**2)
-    array = f(array)
-    pq_val = types.pquant("power", "µW")
-    pq_x = types.pquant("x position", "µm")
-    pq_y = types.pquant("y position", "µm")
-    data = mdata.MatrixData()
-    data.set_val_data(array, val_pquant=pq_val)
-    data.set_var_data(
-        pq_pxsize_x=6.45, pq_pxsize_y=6.45, pq_offset_x=0.0, pq_offset_y=0.0,
-        var_x_pquant=pq_x, var_y_pquant=pq_y
-    )
-    print(data)
-    # FFT
-    ft_data = fft_matrixdata(data, mode="abs")
-    print(ft_data)
 
     # ----------
     # Array data
--- libics/trafo/fourierutil.py
+++ libics/trafo/fourierutil.py
@@ -1,7 +1,5 @@
 import copy
 
-from libics.data import matrixdata as mdata
-
 
 ###############################################################################
 
@@ -28,40 +26,6 @@ def ft_pquants(pq_val, *pq_var):
     for pq in pq_var:
         pq.invert()
     return pq_val, pq_var
-
-
-def ft_matrixrect(mrect):
-    """
-    Calculates the Fourier transformed coordinates of a
-    `data.matrixdata.MatrixRect`.
-
-    Parameters
-    ----------
-    mrect : data.matrixdata.MatrixRect
-        Matrix rectangle before Fourier transformation.
-
-    Returns
-    -------
-    ft_mrect : data.matrixdata.MatrixRect
-        Matrix rectangle after Fourier transformation.
-    """
-    # Calculate FT pixel sizes
-    pxsize_x, pxsize_y = mrect.get_pxsize()
-    pxcount_x, pxcount_y = mrect.get_pxcount()
-    f_nyq_x, f_nyq_y = 1 / 2 / pxsize_x, 1 / 2 / pxsize_y
-    f_cor_x, f_cor_y = 0.0, 0.0
-    # Even point correction (acc. to FFT implementation)
-    if pxcount_x % 2 == 0:
-        f_cor_x = f_nyq_x / (pxcount_x / 2)
-    if pxcount_y % 2 == 0:
-        f_cor_y = f_nyq_y / (pxcount_y / 2)
-    # Create FT matrix rectangle
-    ft_mrect = mdata.MatrixRect()
-    ft_mrect.ind_bound_x = mrect.ind_bound_x
-    ft_mrect.ind_bound_y = mrect.ind_bound_y
-    ft_mrect.pquant_bound_x = [-f_nyq_x, f_nyq_x - f_cor_x]
-    ft_mrect.pquant_bound_y = [-f_nyq_y, f_nyq_y - f_cor_y]
-    return ft_mrect
 
 
 def ft_arrayscale(scale, shape):
