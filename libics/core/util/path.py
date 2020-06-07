9407d7a8799c23880b64dbf90157edee520ef2f2
David Wei <david94.wei@gmail.com>
2020-06-07 19:15:07 +0200

    core.util.path: Add FolderContentsResult

--- libics/core/util/path.py
+++ libics/core/util/path.py
@@ -5,6 +5,60 @@ import os
 ###############################################################################
 
 
+class FolderContentsResult(object):
+
+    """
+    Results container for :py:func:`get_folder_contents`.
+
+    Attributes
+    ----------
+    regex : `str`
+        Matching regex.
+    path : `str`
+        Parent directory path used for search.
+    folders : `list(str)`
+        Full list of subfolders.
+    files : `list(str)`
+        Full list of files within folder.
+    folders_matched : `list(str)`
+        List of matched folders.
+    files_matched : `list(str)`
+        List of matched files.
+    folders_unmatched : `list(str)`
+        List of unmatched folders.
+    files_unmatched : `list(str)`
+        List of unmatched files.
+    """
+
+    def __init__(self, **kwargs):
+        self.regex = ""
+        self.path = ""
+        self.folders_matched = []
+        self.files_matched = []
+        self.folders_unmatched = []
+        self.files_unmatched = []
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+
+    @property
+    def folders(self):
+        return self.folders_matched + self.folders_unmatched
+
+    @property
+    def files(self):
+        return self.files_matched + self.files_unmatched
+
+    def __iter__(self):
+        for item in [
+            self.folders, self.files, self.folders_matched, self.files_matched,
+            self.folders_unmatched, self.files_unmatched
+        ]:
+            yield item
+
+    def __getitem__(self, key):
+        return list(self)[key]
+
+
 def get_folder_contents(folder, regex=None):
     """
     Gets the files/subfolders in a folder.
@@ -18,43 +72,35 @@ def get_folder_contents(folder, regex=None):
 
     Returns
     -------
-    folder : list(str)
-        Full list of subfolders.
-    files : list(str)
-        Full list of files within folder.
-    folders_match : list(str)
-        List of subfolders matching regex.
-    files_match : list(str)
-        List of files matching regex.
-    folders_rest : list(str)
-        List of subfolders not matching regex.
-    files_rest : list(str)
-        List of files not matching regex.
+    res : `FolderContentsResult`
+        Result container.
     """
     if regex == "":
         regex = None
-    folders, folders_match, folders_rest = [], [], []
-    files, files_match, files_rest = [], [], []
+    folders_matched, folders_unmatched = [], []
+    files_matched, files_unmatched = [], []
     for item in os.listdir(folder):
         if os.path.isfile(os.path.join(folder, item)):
-            files.append(item)
             try:
                 if regex is not None and re.search(regex, item):
-                    files_match.append(item)
+                    files_matched.append(item)
                 else:
-                    files_rest.append(item)
+                    files_unmatched.append(item)
             except re.error:
-                files_rest.append(item)
+                files_unmatched.append(item)
         else:
-            folders.append(item)
             try:
                 if regex is not None and re.search(regex, item):
-                    folders_match.append(item)
+                    folders_matched.append(item)
                 else:
-                    folders_rest.append(item)
+                    folders_unmatched.append(item)
             except re.error:
-                folders_rest.append(item)
-    return folders, files, folders_match, files_match, folders_rest, files_rest
+                folders_unmatched.append(item)
+    return FolderContentsResult(
+        regex=regex, path=folder,
+        folders_matched=folders_matched, files_matched=files_matched,
+        folders_unmatched=folders_unmatched, files_unmatched=files_unmatched
+    )
 
 
 def assume_file_exists(file_path):
