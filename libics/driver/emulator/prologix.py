eac047bb32dace8126d70e6fb1f535d0ad133575
David Wei <david94.wei@gmail.com>
2020-06-15 23:58:48 +0200

    driver: Update emulator

--- libics/driver/emulator/prologix.py
+++ libics/driver/emulator/prologix.py
@@ -1,19 +1,52 @@
 import time
 
 from libics.driver.emulator import ItfEmulator
-from libics.driver.terminal import gpib
+from libics.driver.device import STATUS
+from libics.driver.terminal import ItfTerminal
+from libics.driver.terminal.gpib import ItfGpib, GPIB_MODE
 
 
 ###############################################################################
 
 
-class PrologixGpib(ItfEmulator, gpib.ItfGpib):
+class PrologixGpib(ItfEmulator, ItfGpib):
 
-    def __init__(self, cfg):
-        super().__init__(cfg)
+    def __init__(self):
+        super().__init__()
+        self._is_set_up = False
+        self._is_connected = False
 
-    def connect(self):
-        super().connect()
+    # ++++++++++++++++++++++++++++++++++++++++
+    # Device methods
+    # ++++++++++++++++++++++++++++++++++++++++
+
+    def setup(self):
+        """
+        Raises
+        ------
+        RuntimeError
+            If interface is not set.
+        """
+        if not isinstance(self.interface, ItfTerminal):
+            err_msg = "invalid interface"
+            self.last_status = STATUS(
+                state=STATUS.ERROR, err_type=STATUS.ERR_CONNECTION, msg=err_msg
+            )
+            raise RuntimeError(err_msg)
+        self.interface.setup()
+        self._is_set_up = True
+
+    def shutdown(self):
+        if self.is_set_up():
+            self.interface.shutdown()
+        self._is_set_up = False
+
+    def is_set_up(self):
+        return self.interface.is_set_up() and self._is_set_up
+
+    def connect(self, id):
+        self.interface.connect(self.identifier)
+        self.interface.register(self.identifier, self)
         # Disable in-device cfg saving
         self.send("++savecfg 0")
         # Set GPIB mode
@@ -31,13 +64,32 @@ class PrologixGpib(ItfEmulator, gpib.ItfGpib):
         self.send("++read_tmo_ms {:d}"
                   .format(round(1000 * self.cfg.recv_timeout)))
         time.sleep(2)
+        self._is_connected = True
 
     def close(self):
         # Set local
         self.send("++loc")
-        super().close()
+        self.interface.deregister(id=self.identifier)
+        self.interface.close()
+        self._is_connected = False
+
+    def is_connected(self):
+        return self.interface.is_connected() and self._is_connected
+
+    # ++++++++++++++++++++++++++++++++++++++++
+    # ItfTerminal methods
+    # ++++++++++++++++++++++++++++++++++++++++
+
+    def send(self, msg):
+        return self.interface.send(msg)
 
     def recv(self):
-        if self.cfg.gpib_mode == self.GPIB_MODE.CONTROLLER:
+        if self.mode == GPIB_MODE.CONTROLLER:
             self.send("++read")
-        return super().recv()
+        return self.interface.recv()
+
+    def flush_out(self):
+        return self.interface.flush_out()
+
+    def flush_in(self):
+        return self.interface.flush_in()
