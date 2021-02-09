28b7a4152ee45cd72a7f5d4498e1bce5fad01baf
David Wei <david94.wei@gmail.com>
2021-02-09 19:10:37 +0100

    tools.math.flat: Add FitLinear1d

--- libics/tools/math/flat.py
+++ libics/tools/math/flat.py
@@ -1,5 +1,8 @@
 import numpy as np
 
+from libics.env import logging
+from libics.tools.math.models import ModelBase
+
 
 ###############################################################################
 # Oscillating Functions
@@ -15,3 +18,48 @@ def cosine_2d(
         + amplitude_y * np.cos(2 * np.pi * var[1] / period_y + phase_y)
         + offset
     )
+
+
+###############################################################################
+# Monotonic Functions
+###############################################################################
+
+
+def linear_1d(x, a, c=0.0):
+    r"""
+    Linear in one dimension.
+
+    .. math::
+        a x + c
+    """
+    return a * x + c
+
+
+class FitLinear1d(ModelBase):
+
+    """
+    Fit class for :py:func:`linear_1d`.
+
+    Parameters
+    ----------
+    a (amplitude)
+    c (offset)
+    """
+
+    LOGGER = logging.get_logger("libics.tools.math.flat.FitLinear1d")
+    P_ALL = ["a", "c"]
+    P_DEFAULT = [1, 0]
+
+    @staticmethod
+    def _func(var, *p):
+        return linear_1d(var, *p)
+
+    def find_p0(self, *data):
+        var_data, func_data = self._split_fit_data(*data)
+        var_data = var_data.ravel()
+        idx_min, idx_max = np.argmin(func_data), np.argmax(func_data)
+        fmin, fmax = func_data[idx_min], func_data[idx_max]
+        vmin, vmax = var_data[idx_min], var_data[idx_max]
+        a = (fmax - fmin) / (vmax - vmin)
+        c = fmax - a * vmax
+        self.p0 = [a, c]
